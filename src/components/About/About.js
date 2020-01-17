8eac78b0938d5b4dd7a695c0416c05bf2530bff2
Abhishek K <abhishekakade@gmail.com>
2020-01-17 22:48:44 +0530

    chore: updated info

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -5,25 +5,30 @@ const About = () => {
   return (
     <section id="about">
       <h3 className="title">About Me</h3>
+      <hr className="hr-green" />
       <p>
         Hello! I'm Abhishek, a Front End Developer based in Karad, Maharashtra
-        who enjoys building stuff with JavaScript and React. I develop
-        responsive websites and web apps that are optimized for speed so they
-        can perform well on all devices even on slower networks.
+        who enjoys building stuff with JavaScript and React. I develop{" "}
+        <span className="highlight-text-white">
+          fast, responsive and accessible websites/web apps
+        </span>{" "}
+        which perform well on all devices, even on slower networks.
       </p>
       <p>
         Computers have always fascinated me. I'm a dentist but I first dabbled
-        into web development out of interest in web, the Internet and
-        programming. I started reading about web development in my final year of
-        dentistry and then started learning web development in internship from
-        resources like freeCodeCamp, YouTube, MDN, Reddit, Medium, etc.
+        into web development out of interest in the Internet. I wanted to learn
+        more about how everything was made available so easily for anyone to
+        access from any part of the world. I started reading about web
+        development in my final year of dentistry and then actually started
+        learning web development in internship from resources like freeCodeCamp,
+        YouTube, MDN, Reddit, Medium, etc.
         {/* Then it became something more than just interest. It became my passion.  */}
       </p>
       <p>
-        Around that time, I got to know about Google India Scholarship. I
-        applied, worked hard and got selected in the top 350 out of 10k
-        applicants all over India to receive Google-Udacity's Front-End
-        Nanodegree. I successfully{" "}
+        Later, I got to know about Google India Scholarship. I applied, worked
+        hard and got selected in the top 350 out of 10k applicants all over
+        India to receive Google-Udacity's Front-End Nanodegree. This was HUGE
+        for me and it changed my life forever! I successfully{" "}
         <a
           href="https://graduation.udacity.com/confirm/DEJDC66D"
           target="_blank"
