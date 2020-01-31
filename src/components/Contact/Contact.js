6487ef6c5aacd36d34f0430eb53eb8d4c0756fd1
Abhishek K <abhishekakade@gmail.com>
2020-01-31 23:31:18 +0530

    fix: added mailto address, styles

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,16 +1,20 @@
 import React from "react"
-
+import "./Contact.css"
 const Contact = () => {
   return (
     <section id="contact">
       {/* <div> */}
-      <h3 className="title">Contact</h3>
+      <h3 className="title">Get In Touch</h3>
       <hr className="hr-green" />
       <p>
         Feel free to send me an email any time, whether it is about freelance
         work or a project or just to talk about web development. I'll get back
         to you as soon as possible.
       </p>
+
+      <a className="get-in-touch-btn" href="mailto:abhishekakade@gmail.com">
+        Say Hi!
+      </a>
       {/* </div> */}
     </section>
   )
