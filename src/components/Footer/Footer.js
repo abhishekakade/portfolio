857d9eb5b64b2d9f37cdedd56bc03c6dcba965e7
Abhishek K <abhishekakade@gmail.com>
2020-01-31 23:30:22 +0530

    chore: added footer section

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,11 @@
+import React from "react"
+import "./Footer.css"
+
+export const Footer = () => {
+  return (
+    <footer>
+      <p>Made with love using Gatsby</p>
+      <p>Design by Brittany Chiang</p>
+    </footer>
+  )
+}
