3f6fb6665a8026fc23103157b05c3269dc7f8ec8
Abhishek K <abhishekakade@gmail.com>
2020-01-31 23:32:20 +0530

    chore: adding projects

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -6,27 +6,33 @@ const Projects = () => {
     <section id="about">
       <h3 className="title">Projects</h3>
       <hr className="hr-green" />
-      <p>
-        Hello! I'm Abhishek, a Front End Developer based in Karad, Maharashtra
-        who enjoys building stuff with JavaScript and React. I develop{" "}
-        <span className="highlight-text-white">
-          fast, responsive and accessible websites/web apps
-        </span>{" "}
-        which perform well on all devices, even on slower networks.
-      </p>
-
-      <ul className="skill-list mono">
-        <li className="skill-list-item">JavaScript (ES6+)</li>
-        <li className="skill-list-item">TypeScript</li>
-        <li className="skill-list-item">ReactJS, GatsbyJS</li>
-        <li className="skill-list-item">HTML5, CSS3, SCSS</li>
-        <li className="skill-list-item">Progressive Web Apps (PWA)</li>
-        <li className="skill-list-item">Performance Optimizations</li>
-        <li className="skill-list-item">Accessibility</li>
-        <li className="skill-list-item">Responsive Layouts</li>
-        <li className="skill-list-item">Some Node, Express, MongoDB</li>
-        <li className="skill-list-item">Some Python and Golang</li>
-      </ul>
+      <div className="project">
+        <h4>Featured Project</h4>
+        <h5>Flickk</h5>
+        <p>
+          See which movies are trending, their ratings, description, cast,
+          genres and more. You can even search for all the movies available in
+          the{" "}
+          <a
+            href="https://www.themoviedb.org/"
+            target="_blank"
+            rel="noopener noreferrer nofollow"
+          >
+            Movie Database
+          </a>
+          !
+        </p>
+      </div>
+      <div className="project">
+        <h4>Featured Project</h4>
+        <h5>Project Name</h5>
+        <p>Project info.</p>
+      </div>
+      <div className="project">
+        <h4>Featured Project</h4>
+        <h5>Project Name</h5>
+        <p>Project info.</p>
+      </div>
     </section>
   )
 }
