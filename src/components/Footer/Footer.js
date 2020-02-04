47b65da1505ecf9f909e191e8617acbf3eacb63c
Abhishek K <abhishekakade@gmail.com>
2020-02-04 23:02:06 +0530

    feat: footer completed

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,11 +1,66 @@
 import React from "react"
+import { Github, Linkedin, Codepen } from "styled-icons/icomoon/"
+import { Heart } from "styled-icons/fa-solid/"
 import "./Footer.css"
 
-export const Footer = () => {
+const Footer = () => {
   return (
     <footer>
-      <p>Made with love using Gatsby</p>
-      <p>Design by Brittany Chiang</p>
+      <ul id="footer-social-links">
+        <li>
+          <a
+            href="https://www.linkedin.com/in/abhishek-kakade/"
+            target="_blank"
+            rel="nofollow noopener noreferrer"
+            aria-label="Abhishek's LinkedIn Profile"
+            className="tooltip"
+          >
+            <Linkedin className="icons" />
+            <span className="tooltiptext">LinkedIn</span>
+          </a>
+        </li>
+        <li>
+          <a
+            href="https://github.com/abhishekakade"
+            target="_blank"
+            rel="nofollow noopener noreferrer"
+            aria-label="Abhishek's GitHub Profile"
+            className="tooltip"
+          >
+            <Github className="icons" />
+            <span className="tooltiptext">GitHub</span>
+          </a>
+        </li>
+        <li>
+          <a
+            href="https://codepen.io/abhinkakade/"
+            target="_blank"
+            rel="nofollow noopener noreferrer"
+            aria-label="Abhishek's CodePen Profile"
+            className="tooltip"
+          >
+            <Codepen className="icons" />
+            <span className="tooltiptext">CodePen</span>
+          </a>
+        </li>
+      </ul>
+      <p>
+        Made with&nbsp; {<Heart id="heart-icon" />} &nbsp;using{" "}
+        <a href="https://www.gatsbyjs.org">Gatsby</a>!
+      </p>
+      <p id="design-by">
+        Design -{" "}
+        <a
+          href="https://github.com/bchiang7/v4"
+          target="_blank"
+          rel="nofollow noopener noreferrer"
+          aria-label="Link to Brittany Chiang's Gatsby Theme"
+        >
+          Brittany Chiang
+        </a>
+      </p>
     </footer>
   )
 }
+
+export default Footer
