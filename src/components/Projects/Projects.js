7004616b2da14334056f7b764bc7075eba77c5fc
Abhishek K <abhishekakade@gmail.com>
2020-02-24 23:46:52 +0530

    chore: added more projects

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -9,30 +9,51 @@ const Projects = () => {
       <div className="project">
         <h4>Featured Project</h4>
         <h5>Flickk</h5>
+        <h6>Movies Info App</h6>
         <p>
-          See which movies are trending, their ratings, description, cast,
-          genres and more. You can even search for all the movies available in
-          the{" "}
+          PWA built with ReactJS &{" "}
           <a
             href="https://www.themoviedb.org/"
             target="_blank"
             rel="noopener noreferrer nofollow"
           >
-            Movie Database
+            TMDb
           </a>
-          !
+          API! that shows all the trending movies, their summary and even cast
+          information. Optimized for performance. 
+          {/* See which movies are trending, their ratings, description, cast,
+          genres and more. You can even search for specific movies available in{" "} */}
         </p>
       </div>
       <div className="project">
         <h4>Featured Project</h4>
-        <h5>Project Name</h5>
-        <p>Project info.</p>
+        <h5>Random Quote Machine</h5>
+        <p>
+          PWA built with ReactJS, uses FavQs API to fetch quotes & has a tweet
+          button to share them. Optimized for performance. 
+        </p>
       </div>
       <div className="project">
         <h4>Featured Project</h4>
+        <h5>Markdown Previewer</h5>
+        <p>
+          PWA built with ReactJS & Marked.js library. Its a markdown editor that
+          features a live preview. Optimized for performance.
+        </p>
+      </div>
+      <div className="project">
+        <h4>Featured Project</h4>
+        <h5>React Piano/Drumkit</h5>
+        <p>
+          PWA built with ReactJS. Features a minimalist piano & drum kit.
+          Optimized for performance. 
+        </p>
+      </div>
+      {/* <div className="project">
+        <h4>Featured Project</h4>
         <h5>Project Name</h5>
         <p>Project info.</p>
-      </div>
+      </div> */}
     </section>
   )
 }
